a3ec878dd57ba7209265c0cfeb77dab1313380c6
NikolayZhulin <nikolayzhulin97@mail.ru>
2023-02-11 16:51:49 +0100

    fix: change app-reducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,5 +1,8 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+import { isErrorWithMessage, isFetchBaseQueryError } from '../common/services/helpers'
+import { authAPI } from '../features/auth/authAPI'
+
 const initialState = {
   status: 'idle' as RequestStatusType,
   error: null as null | string,
@@ -19,6 +22,22 @@ const slice = createSlice({
     setAppErrorAC: (state, action: PayloadAction<{ error: string | null }>) => {
       state.error = action.payload.error
     },
+  },
+  extraReducers: builder => {
+    builder.addMatcher(authAPI.endpoints.registration.matchRejected, (state, { payload }) => {
+      if (isFetchBaseQueryError(payload)) {
+        const errMsg = 'error' in payload ? payload.error : JSON.stringify(payload.data)
+
+        console.log('ERRRORRRRR')
+        state.error = JSON.parse(errMsg).error
+      } else if (isErrorWithMessage(payload)) {
+        console.warn('Unknown Error')
+      }
+      setTimeout(() => {
+        console.log(111111111)
+        state.error = null
+      }, 3000)
+    })
   },
 })
 
