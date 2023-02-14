f2ba7025892883ed8a4d8a21000304581278a11c
Nikita Gaponov <nikitagaponov@gmail.com>
2023-02-14 16:17:10 +0300

    fix: fix Paginator Packs

--- src/features/packs/PaginationPacks.tsx
+++ src/features/packs/PaginationPacks.tsx
@@ -1,45 +1,23 @@
-import { useLayoutEffect, useState } from 'react'
-
-import { Pagination } from 'antd'
-import { useSearchParams } from 'react-router-dom'
-
 import { InitialPreloader } from '../../common/components'
+import { PaginationFC } from '../../common/components/pagination/PaginationFC'
 import { useAppSelector } from '../../common/hooks/hooks'
 import { Login } from '../auth'
 
-import { RequestURIPackType, useLazyGetPacksQuery } from './pagination-api'
-import { PaginationFC } from './PaginationFC'
+import { useLazyGetPacksQuery } from './pagination-api'
 
 export const PaginationPacks = () => {
-  const [params, setParams] = useState<RequestURIPackType>({ page: 1, pageCount: 4 })
-  const [searchParams, setSearchParams] = useSearchParams()
-  const [trigger, result, lastPromiseInfo] = useLazyGetPacksQuery()
+  const [trigger, result] = useLazyGetPacksQuery()
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
-  const data = result.data
-  const getData = trigger
-
-  useLayoutEffect(() => {
-    const params = Object.fromEntries(searchParams)
-    const fetchData = async () => {
-      setParams({
-        page: +params.page,
-        pageCount: +params.pageCount,
-      })
-    }
-
-    fetchData().then(() => {
-      trigger(params)
-    })
-  }, [])
-
-  const onChangePaginationHandler = (page: number, pageCount: number) => {
-    setParams({ page, pageCount })
-    setSearchParams({ page: page.toString(), pageCount: pageCount.toString() })
-    trigger({ page, pageCount })
-  }
 
   if (!isLoggedIn) return <Login />
   if (result.isLoading) return <InitialPreloader />
 
-  return <PaginationFC data={data} getData={getData} />
+  return (
+    <PaginationFC
+      pageSize={result.data?.pageCount || 4}
+      total={result.data?.cardPacksTotalCount || 100}
+      current={result.data?.page || 1}
+      onChange={trigger}
+    />
+  )
 }
