c53aa2eca834fd93a8f4354f532098b0ce07e7f6
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-02-21 15:35:05 +0300

    fix: add condition

--- src/features/learn/utils/multiplyGradesPush.ts
+++ src/features/learn/utils/multiplyGradesPush.ts
@@ -1,5 +1,13 @@
-export const multiplyGradesPush = (value: number, pushQty: number, state: any) => {
-  for (let i = 0; i < pushQty; i++) {
-    state.grades.push(value)
+export const multiplyGradesPush = (grade: number, state: any) => {
+  let qty
+
+  if (grade === 0) {
+    qty = 20
+  } else {
+    qty = Math.round(20 / grade)
+  }
+
+  for (let i = 0; i < qty; i++) {
+    state.grades.push(grade)
   }
 }
