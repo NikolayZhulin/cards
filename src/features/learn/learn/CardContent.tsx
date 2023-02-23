abaefa47173bed8891b24322eb6863bde9d7e96c
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-02-24 00:45:16 +0300

    fix: relocation pack name

--- src/features/learn/learn/CardContent.tsx
+++ src/features/learn/learn/CardContent.tsx
@@ -1,19 +1,13 @@
 import React, { memo } from 'react'
 
-import { useAppSelector } from '../../../common/hooks/reduxHooks'
-
 type Props = {
   question: string
   shots: number
 }
 
 export const CardContent = memo(({ question, shots }: Props) => {
-  const packName = useAppSelector(state => state.learn.packName)
-
   return (
     <>
-      <h1>{packName}</h1>
-
       <h2 style={{ wordWrap: 'break-word' }}>Question: {question}</h2>
       <p style={{ color: 'gray' }}>Количество попыток ответов на вопрос: {shots}</p>
     </>
--- src/features/learn/learn/index.tsx
+++ src/features/learn/learn/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
+import { useAppSelector } from '../../../common/hooks/reduxHooks'
 import { BackToPacksButton } from '../../tables/table/Cards/cards-blocks/BackToPacksButton'
 import { LearnCardWrapper, LearnStyledCard, LearnWrapper } from '../styles'
 
 import { Card } from './Card'
 
 export const Learn = () => {
+  const packName = useAppSelector(state => state.learn.packName)
+
   return (
     <LearnWrapper>
       <BackToPacksButton />
       <LearnCardWrapper>
+        <h1>{packName}</h1>
         <LearnStyledCard>
           <Card />
         </LearnStyledCard>
