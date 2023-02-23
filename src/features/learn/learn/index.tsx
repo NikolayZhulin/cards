615e00cf173b7db3403d4608d4f760609efaa8b0
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-02-24 00:40:33 +0300

    fix: change import path

--- src/features/learn/learn/index.tsx
+++ src/features/learn/learn/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { BackToPacksButton } from '../../tables/table/Cards/cards-blocks/BackToPacksButton'
 import { LearnCardWrapper, LearnStyledCard, LearnWrapper } from '../styles'
 
-import { Card } from './CardCopy'
+import { Card } from './Card'
 
 export const Learn = () => {
   return (
