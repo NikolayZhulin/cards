c74b021fe48f3c569c8457ff623bcd7f2bcd6fd7
Nikita Gaponov <nikitagaponov@gmail.com>
2023-02-27 09:00:17 +0300

    feat: create useAutoFocus

--- src/common/components/modal/UpdatePackModal.tsx
+++ src/common/components/modal/UpdatePackModal.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 
-import NiceModal, { useModal } from '@ebay/nice-modal-react'
+import { create, useModal } from '@ebay/nice-modal-react'
 import { Input } from 'antd'
 
 import { useLazyFetchCardsQuery } from '../../../features/cards'
 import { useUpdatePackMutation } from '../../../features/packs'
+import { useAutoFocus } from '../../hooks/useAutoFocus'
 
 import { ModalFC } from './ModalFC'
 
@@ -13,25 +14,18 @@ type Props = {
   cardsPack_id?: string
 }
 
-export const UpdatePackModal = NiceModal.create(({ prevName, cardsPack_id }: Props) => {
+export const UpdatePackModal = create(({ prevName, cardsPack_id }: Props) => {
   const modal = useModal()
   const [updatePack, { isLoading: packIsUpdating }] = useUpdatePackMutation()
   const [value, setValue] = useState<string | undefined>(prevName)
   const [trigger, response] = useLazyFetchCardsQuery()
+  const inputTagRef = useAutoFocus()
   const updatePackHandler = async () => {
     await updatePack({ cardsPack: { _id: cardsPack_id, name: value } })
     trigger({ cardsPack_id: cardsPack_id })
     modal.hide()
     setValue('')
   }
-  const inputTagRef = useRef(null)
-
-  useEffect(() => {
-    if (inputTagRef.current) {
-      // @ts-ignore
-      inputTagRef.current.focus()
-    }
-  }, [inputTagRef.current])
 
   return (
     <ModalFC
