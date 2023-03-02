e87bc1caf31c4e73a505eb2845ce45c5b3704707
NikolayZhulin <nikolayzhulin97@mail.ru>
2023-03-02 19:35:05 +0100

    fix: fix modals

--- src/features/packs/hooks/use-pack-list.tsx
+++ src/features/packs/hooks/use-pack-list.tsx
@@ -3,9 +3,9 @@ import React, { useEffect } from 'react'
 import { show } from '@ebay/nice-modal-react'
 import { NavLink, useNavigate } from 'react-router-dom'
 
+import { LearnButton } from '../../../common/components/learn-buttons/LearnButton'
 import { DeletePackModal } from '../../../common/components/modal/DeletePackModal'
 import { UpdatePackModal } from '../../../common/components/modal/UpdatePackModal'
-import { LearnButton } from '../../../common/components/learn-buttons/LearnButton'
 import { UpdateButtons } from '../../../common/components/update-buttons/UpdateButtons'
 import { useAppSelector } from '../../../common/hooks/reduxHooks'
 import { useSearch } from '../../../common/hooks/useSearch'
@@ -16,8 +16,6 @@ import {
   useLazyFetchCardsPackQuery,
   useUpdatePackMutation,
 } from '../packsApi'
-
-import { LearnButton } from 'common/components/learn-buttons/LearnButton'
 
 export const usePackList = () => {
   const userId = useAppSelector(state => state.auth.userId)
